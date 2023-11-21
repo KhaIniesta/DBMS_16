8741f3f88217ff2271d655762e675d09b23dd07c
KhaIniesta <le.minh.kha.student.hcmute@gmail.com>
2023-11-21 16:39:45 +0700

    [kha] loc cac phan khong dung

--- views.sql
+++ views.sql
@@ -2,17 +2,6 @@
 use QLNhaSach
 -- 1. Xem các thông tin sách trong kho
 GO
-CREATE VIEW V_ThongTinSachTrongKho AS
-SELECT s.MaTG, s.MaNXB, s.TenSach, s.SoLuongSach, s.Gia, s.TheLoai , ctpn.MaPhieuNhap, ctpn.SoLuongNhap
-FROM Sach s INNER JOIN ChiTietPhieuNhap ctpn ON s.MaSach = ctpn.MaSach
-GO
-
--- 2. Xem chi tiết các hóa đơn
-CREATE VIEW V_ChiTietCacHoaDon AS
-SELECT hd.TongHD, hd.NgayInHD, cthd.MaSach, cthd.SoLuongBan, cthd.Gia
-FROM HoaDon hd INNER JOIN ChiTietHoaDon cthd ON hd.MaHD = cthd.MaHD
-GO
-
 -- 3. Xem chi tiết các phiếu nhập
 CREATE VIEW V_ChiTietCacPhieuNhap AS
 SELECT pn.MaPhieuNhap, pn.MaNXB, pn.NgayNhap, ctpn.MaSach, ctpn.SoLuongNhap
@@ -33,12 +22,6 @@ go
 -- 4.c View xuat tong doanh thu theo nam
 create view V_DTNam as
 select MaHD, Year(NgayInHD) Nam, Month(NgayInHD) Thang, TongHD from HoaDon
-go
-
--- 5. View xem so luong sach da ban trong ngay 
-create view V_SoLuongSachBanTrongNgay as
-select ChiTietHoaDon.MaSach,sum(SoLuongBan) TongSoLuongBan from HoaDon join ChiTietHoaDon on HoaDon.MaHD = ChiTietHoaDon.MaHD 
-where (select cast(NgayInHD as date) ngayInHD from HoaDon) = cast(GetDate() as date) group by ChiTietHoaDon.MaSach
 go
 
 -- 6. View hiển thị chi tiết sách trong chi tiết hóa đơn
